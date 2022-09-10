528cf6f57bc52e7454fe756479907fe941a5883b
jaychang99 <jaychang99@gmail.com>
2022-09-10 22:32:01 +0900

    fix(frontend): PageLayout 컴포넌트에서 StyledPageLayout 에 ...props 가 넘어가지 않던 문제 수정

--- src/components/common/Layout/PageLayout/index.tsx
+++ src/components/common/Layout/PageLayout/index.tsx
@@ -4,7 +4,7 @@ import { StyledPageLayout } from "components/common/Layout/PageLayout/styles";
 interface Props extends HTMLAttributes<HTMLDivElement> {}
 
 function PageLayout({ children, ...props }: Props) {
-  return <StyledPageLayout>{children}</StyledPageLayout>;
+  return <StyledPageLayout {...props}>{children}</StyledPageLayout>;
 }
 
 export default PageLayout;
